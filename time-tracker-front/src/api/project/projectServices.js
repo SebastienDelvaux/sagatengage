bd00ece3ad1fda599d2935c64345f0a98438f89e
Sébastien Delvaux <delvaux.sebastien@protonmail.com>
2018-08-17 14:35:59 +0200

    Creating back-end end-point to fetch project data (/project/:id). Creating front-end API call to fetch those data

--- time-tracker-front/src/api/project/projectServices.js
+++ time-tracker-front/src/api/project/projectServices.js
@@ -16,3 +16,18 @@ export const getProjects = async () => {
     console.error('Error while fetching the project list', error);
   }
 }
+
+export const getProject = async id => {
+  try {
+    const response = await fetch(`${baseUrl}/project/${id}`, {
+      method: 'get',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    console.error(`Error while fetching the project with ID '${id}'`, error);
+  }
+}
--- time-tracker-front/src/project/Project.jsx
+++ time-tracker-front/src/project/Project.jsx
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+import {getProject} from '../api/project/projectServices';
+
 export default class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {project: {}};
+  }
+
+  async componentDidMount() {
+    const data = await getProject(this.props.id);
+    this.setState({project: data});
+  }
+
   render() {
     return (
       <div>
         <nav>
           <Link to="/">Project overview</Link>
         </nav>
-        <p>Showing project with id {this.props.id}</p>
+        <h3>{this.state.project.title}</h3>
+        <p>{this.state.project.description}</p>
       </div>
     );
   }
